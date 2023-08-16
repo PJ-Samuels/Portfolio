6dc60ac5645a5bcbe7d9ae8be2aa2c83255b9707
Oliver Samuels <osamuels@bu.edu>
2023-08-16 13:08:27 -0400

    images additions

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 $(document).ready(function() {
-    var $projectCards = $(".project-card");
+    var $projectCardLeft = $(".project-card-left");
+    var $projectCardRight = $(".project-card-right");
     
     $(window).on("scroll", function() {
         var windowHeight = $(window).height();
         var scrollTop = $(window).scrollTop();
 
-        $projectCards.each(function() {
+        $projectCardLeft.each(function() {
             var cardTop = $(this).offset().top;
             var cardHeight = $(this).outerHeight();
 
             if (cardTop <= scrollTop + windowHeight && cardTop + cardHeight >= scrollTop) {
                 $(this).addClass("animate-left");
-                $(this).addClass("animate-right");
 
             } else {
                 $(this).removeClass("animate-left");
+            }
+        });
+        $projectCardRight.each(function() {
+            var cardTop = $(this).offset().top;
+            var cardHeight = $(this).outerHeight();
+
+            if (cardTop <= scrollTop + windowHeight && cardTop + cardHeight >= scrollTop) {
+                $(this).addClass("animate-right");
+
+            } else {
                 $(this).removeClass("animate-right");
             }
         });
